eab242ede9a125a4b8ae17a0bb29a5e69803bc22
Gorka Hernández Estomba <info@gorkahernandez.com>
2021-06-28 22:14:05 +0200

    update theme

--- src/app/components/post-preview.component.ts
+++ src/app/components/post-preview.component.ts
@@ -3,6 +3,16 @@ import { Component, Input } from "@angular/core";
 @Component({
     selector: 'app-post-preview',
     template: ``,
-    styles: [``]
+    styles: [`
+        /**
+         * Suggested styles
+         */
+        .post-title {
+            margin-bottom: 5px;
+        }
+        .post-body {
+            margin-top: 0;
+        }
+    `]
 })
 export class PostPreviewComponent {}
